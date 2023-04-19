ca41b1655d64ee9b9b3b5b76dbb048515c043a77
Vinicius Brigido <aluno@tub-depe-b0804.tubarao.ifsc.edu.br>
2023-04-18 21:23:15 -0300

    Adicionado limite na busca de registros do thingsboard

--- src/main/java/com/ifsc/julio/javatcc/rest/ThingsBoardRest.java
+++ src/main/java/com/ifsc/julio/javatcc/rest/ThingsBoardRest.java
@@ -14,6 +14,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import static com.ifsc.julio.javatcc.util.Const.*;
 import static java.lang.String.*;
 import static java.util.Objects.nonNull;
 import static org.springframework.http.HttpMethod.*;
@@ -43,6 +44,7 @@ public class ThingsBoardRest {
                 .queryParam("keys", deviceSearch.getKeysString())
                 .queryParam("startTs", deviceSearch.getStartMiliseconds())
                 .queryParam("endTs", deviceSearch.getEndMiliseconds())
+                .queryParam("limit", REGISTER_LIMIT)
                 .build()
                 .toUri();
 
--- src/main/java/com/ifsc/julio/javatcc/util/Const.java
+++ src/main/java/com/ifsc/julio/javatcc/util/Const.java
@@ -4,4 +4,5 @@ public class Const {
 
     public static final String TEMPERATURE = "temperature";
     public static final String HUMIDITY = "humidity";
+    public static final Integer REGISTER_LIMIT = 10000;
 }
